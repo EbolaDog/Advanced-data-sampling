2c1d54e08e472c8ee9a84aa67a20a49f70e7e11d
MaximF <adm.f.maxim@yandex.ru>
2023-04-17 12:05:41 +0300

    last fix

--- INSERT.sql
+++ INSERT.sql
@@ -1,17 +1,14 @@
-select name, yearofissue from album
-where yearofissue = 2018;
+insert into genres
+values (1, 'Rap'), (2, 'Rock'), (3, 'pop music'), (4, 'blues'), (5, 'Phonk');
 
-select name, duration from track
-where duration=(select max(duration) from track);
+insert into executor(name)
+values ('eminem'), ('Король и Шут'),('Britney Spears'),('B. B. King'),('Gary Clark Jr.'),('50 cent'),('system of a down'), ('LXST CXNTURY');
 
-select name from track
-where duration >= 210;
+insert into albums(name,yearofissue)
+values ('The Eminem Show', 2002), ('Тень клоуна', 2008), ('Glory', 2016), ('Live', 2008), ('110', 2004), ('The Kanan Tape', 2015), ('Toxicity', 2001), ('Universe of the Past ', 2019);
 
-select distinct name from collection
-where yearofissue between 2018 and 2020;
+insert into tracks(albums_id, name, duration) 
+values (5, 'Tokyo', 209), (5, 'Star Way', 248), (2, 'Chop Suey', 329), (2, 'Aerials', 404), (1, 'Cndy Shop ', 410), (1, 'In Da Club', 411), (4, 'Come Together', 428), (4, ' This Land', 640), (4,'The Thrll is Gone', 642), (4, 'Sweet Little Angel', 553), (3, 'I m A Slave 4 U', 324), (3, 'Baby One More Time', 357), (2, 'Лесник', 323), (2, 'Прыгнул со скалы', 228), (1, 'Wothout Me', 458), (1, 'Not Afraid', 419);
 
-select name from executor
-where name not like '% %'
-
-select name from track
-where name like '%My%' or name like '%my%' or name like '%мой%' or name like '%Мой%'
+insert into collections(id, name, yearofissue)
+values (1, 'The Best', 2015), (2, 'New', 2023), (3, 'Top Chart 2010', 2010), (4, 'Top Chart 2011', 2011), (5, 'Top Chart 2012', 2012), (6, 'Top Chart 2020', 2020), (7, 'Top Chart 2022', 2022), (8, 'Top Chart 2018', 2018);
--- SELCT.sql
+++ SELCT.sql
@@ -28,26 +28,26 @@ join executoralbums on albums.id = executoralbums.albums_id
 join executor on executor.id = executoralbums.executor_id
 where executor.name like '%Король и Шут%'
 
-select albums.name from executor    ---------
-join genresexecutor on executor.id = genresexecutor.executor_id
-join genres on genres.id = genresexecutor.genres_id
+select albums.name from executor
+join genreexecutor on executor.id = genreexecutor.executor_id
+join genres on genres.id = genreexecutor.genres_id
 join executoralbums on executor.id = executoralbums.executor_id
 join albums on albums.id = executoralbums.albums_id
 group by albums.name, executor.name
 having count(genres.name) > 1
-order by count(genres.name) desc    ----------
+order by count(genres.name) desc
 
 select tracks.name from collections
 full join collectionstracks on collections.id = collectionstracks.collections_id
 full join tracks on tracks.id = collectionstracks.tracks_id
 where collections.name is null
 
-select executor.name, tracks.duration from tracks       ----------------
+select executor.name, tracks.duration from tracks ----------------
 join albums on tracks.albums_id = albums.id
-join executoralbums on albums.id = executoralbum.albums_id 
+join executoralbums on albums.id = executoralbums.albums_id
 join executor on executor.id = executoralbums.executor_id
 where tracks.duration = (select min(tracks.duration) from tracks)
-order by tracks.duration                                            -------------------
+order by tracks.duration
 
 select albums.name from albums
 join tracks on tracks.albums_id = albums.id 
